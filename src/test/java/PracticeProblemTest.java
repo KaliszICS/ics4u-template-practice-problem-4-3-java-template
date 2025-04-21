215cfc101a94648962ea7e02c555e005c01c7462
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-21 18:46:36 +0000

    updates

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -15,6 +15,30 @@ public class PracticeProblemTest {
     }
 
     @Test
+    public void testRectangleGetters() {
+        try {
+            Class<?> rectangleClass = Class.forName("Rectangle");
+            Constructor<?> constructor = rectangleClass.getDeclaredConstructor(double.class, double.class);
+            Object rectangle = constructor.newInstance(7.5, 15.0);
+            
+            Method getLengthMethod = rectangleClass.getDeclaredMethod("getLength");
+            Method getWidthMethod = rectangleClass.getDeclaredMethod("getWidth");
+            
+            double length = (double) getLengthMethod.invoke(rectangle);
+            double width = (double) getWidthMethod.invoke(rectangle);
+            
+            assertEquals(7.5, length, 0.01, "getLength() should return the correct value");
+            assertEquals(15.0, width, 0.01, "getWidth() should return the correct value");
+        } catch (ClassNotFoundException e) {
+            fail("Rectangle class does not exist");
+        } catch (NoSuchMethodException e) {
+            fail("Rectangle class is missing a required method: " + e.getMessage());
+        } catch (Exception e) {
+            fail("Error testing Rectangle getters: " + e.getMessage());
+        }
+    }
+
+    @Test
     public void testRectangleNoArgConstructor() {
         try {
             Class<?> rectangleClass = Class.forName("Rectangle");
@@ -27,8 +51,8 @@ public class PracticeProblemTest {
             double length = (double) getLengthMethod.invoke(rectangle);
             double width = (double) getWidthMethod.invoke(rectangle);
             
-            assertEquals(4.0, length, "No-arg constructor should set length to 4", 0.01);
-            assertEquals(8.0, width, "No-arg constructor should set width to 8", 0.01);
+            assertEquals(4.0, length, 0.01, "No-arg constructor should set length to 4");
+            assertEquals(8.0, width, 0.01, "No-arg constructor should set width to 8");
         } catch (ClassNotFoundException e) {
             fail("Rectangle class does not exist");
         } catch (NoSuchMethodException e) {
@@ -51,8 +75,8 @@ public class PracticeProblemTest {
             double length = (double) getLengthMethod.invoke(rectangle);
             double width = (double) getWidthMethod.invoke(rectangle);
             
-            assertEquals(10.0, length, "Single-arg constructor should set length to the provided value", 0.01);
-            assertEquals(8.0, width, "Single-arg constructor should set width to 8", 0.01);
+            assertEquals(10.0, length, 0.01, "Single-arg constructor should set length to the provided value");
+            assertEquals(8.0, width, 0.01, "Single-arg constructor should set width to 8");
         } catch (ClassNotFoundException e) {
             fail("Rectangle class does not exist");
         } catch (NoSuchMethodException e) {
@@ -75,8 +99,8 @@ public class PracticeProblemTest {
             double length = (double) getLengthMethod.invoke(rectangle);
             double width = (double) getWidthMethod.invoke(rectangle);
             
-            assertEquals(7.5, length, "Single-arg constructor should set length to the provided value (7.5)", 0.01);
-            assertEquals(8.0, width, "Single-arg constructor should set width to 8", 0.01);
+            assertEquals(7.5, length, 0.01, "Single-arg constructor should set length to the provided value (7.5)");
+            assertEquals(8.0, width, 0.01, "Single-arg constructor should set width to 8");
         } catch (ClassNotFoundException e) {
             fail("Rectangle class does not exist");
         } catch (NoSuchMethodException e) {
@@ -99,8 +123,8 @@ public class PracticeProblemTest {
             double length = (double) getLengthMethod.invoke(rectangle);
             double width = (double) getWidthMethod.invoke(rectangle);
             
-            assertEquals(5.0, length, "Two-arg constructor should set length to the first provided value", 0.01);
-            assertEquals(12.0, width, "Two-arg constructor should set width to the second provided value", 0.01);
+            assertEquals(5.0, length, 0.01, "Two-arg constructor should set length to the first provided value");
+            assertEquals(12.0, width, 0.01, "Two-arg constructor should set width to the second provided value");
         } catch (ClassNotFoundException e) {
             fail("Rectangle class does not exist");
         } catch (NoSuchMethodException e) {
@@ -123,8 +147,8 @@ public class PracticeProblemTest {
             double length = (double) getLengthMethod.invoke(rectangle);
             double width = (double) getWidthMethod.invoke(rectangle);
             
-            assertEquals(2.5, length, "Two-arg constructor should set length to the first provided value (2.5)", 0.01);
-            assertEquals(6.75, width, "Two-arg constructor should set width to the second provided value (6.75)", 0.01);
+            assertEquals(2.5, length, 0.01, "Two-arg constructor should set length to the first provided value (2.5)");
+            assertEquals(6.75, width, 0.01, "Two-arg constructor should set width to the second provided value (6.75)");
         } catch (ClassNotFoundException e) {
             fail("Rectangle class does not exist");
         } catch (NoSuchMethodException e) {
@@ -134,27 +158,16 @@ public class PracticeProblemTest {
         }
     }
 
-     @Test
-    public void testRectangleGetters() {
+    
+
+    
+
+    @Test
+    public void testChairClassExists() {
         try {
-            Class<?> rectangleClass = Class.forName("Rectangle");
-            Constructor<?> constructor = rectangleClass.getDeclaredConstructor(double.class, double.class);
-            Object rectangle = constructor.newInstance(7.5, 15.0);
-            
-            Method getLengthMethod = rectangleClass.getDeclaredMethod("getLength");
-            Method getWidthMethod = rectangleClass.getDeclaredMethod("getWidth");
-            
-            double length = (double) getLengthMethod.invoke(rectangle);
-            double width = (double) getWidthMethod.invoke(rectangle);
-            
-            assertEquals(7.5, length, "getLength() should return the correct value", 0.01);
-            assertEquals(15.0, width, "getWidth() should return the correct value", 0.01);
+            Class.forName("Chair");
         } catch (ClassNotFoundException e) {
-            fail("Rectangle class does not exist");
-        } catch (NoSuchMethodException e) {
-            fail("Rectangle class is missing a required method: " + e.getMessage());
-        } catch (Exception e) {
-            fail("Error testing Rectangle getters: " + e.getMessage());
+            fail("Chair class does not exist");
         }
     }
 
@@ -179,15 +192,6 @@ public class PracticeProblemTest {
             fail("Chair class is missing a required method: " + e.getMessage());
         } catch (Exception e) {
             fail("Error testing Chair getters: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testChairClassExists() {
-        try {
-            Class.forName("Chair");
-        } catch (ClassNotFoundException e) {
-            fail("Chair class does not exist");
         }
     }
     
